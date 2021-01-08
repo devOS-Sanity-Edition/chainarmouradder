220b2da11b5482c0f7970fd9d99ce3b0bca26cef
Devin Louis <devin@hiitsdevin.dev>
2021-01-08 05:08:01 -0800

    Chainmail Armour Recipe added

--- src/main/java/dev/hiitsdevin/chainarmouradder/Chainarmouradder.java
+++ src/main/java/dev/hiitsdevin/chainarmouradder/Chainarmouradder.java
@@ -1,6 +1,10 @@
 package dev.hiitsdevin.chainarmouradder;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Chainarmouradder implements ModInitializer {
     @Override
