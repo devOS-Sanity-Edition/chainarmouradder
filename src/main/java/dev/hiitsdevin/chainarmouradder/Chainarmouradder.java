5dc7705d1a45063909f17fa4406fe1abe8d44b02
Devin Louis <devin@hiitsdevin.dev>
2021-01-08 04:44:23 -0800

    Initial Commit

---
+++ src/main/java/dev/hiitsdevin/chainarmouradder/Chainarmouradder.java
@@ -0,0 +1,10 @@
+package dev.hiitsdevin.chainarmouradder;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Chainarmouradder implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
